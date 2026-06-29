1bdf5d518047f1a599969ffef057d5acafde9fa2
Anushka <anushkapal232006@gmail.com>
2026-06-29 14:02:11 +0530

    Add files via upload

---
+++ 1STday.py
@@ -0,0 +1,18 @@
+#print("a" and "b" and "c") 
+
+'''a=256
+b=256
+print(a is b)
+
+c=257
+d=257
+print(d is c)
+
+print(True+True)
+print(True+10)
+print(sum([]))'''
+
+def average(numbers):
+    numbers=1
+    return sum(numbers)/ len(numbers)
+    
